2aea85323c6f5684d234c811602a2e2a0a896234
CrishFK <cristiansanchez93@hotmail.es>
2019-06-05 15:20:23 +0200

    NavBar centered and responsive with routelinks Added

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -12,4 +12,13 @@ export class PagesComponent implements OnInit {
   ngOnInit() {
   }
 
+  navBarResponsive(): void {
+    var x = document.getElementById("centered_nav");
+    if (x.className === "rc_nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "rc_nav";
+    }
+  }
+
 }
